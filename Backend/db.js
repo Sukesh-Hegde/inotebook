dedec6850172ff39cabe92474bfb57a34a795ca3
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-07-27 22:02:21 +0530

    initiated react

---
+++ Backend/db.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const url = process.env.DB_URL;
+export const connectUsingMongoose = async () => {
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Mongodb connected using mongoose");
+    addCategories();
+  } catch (err) {
+    console.log("Error while connecting to db");
+    console.log(err);
+  }
+};
+
+async function addCategories() {
+  const CategoryModel = mongoose.model("Category", categorySchema);
+  const categories = CategoryModel.find();
+  if (!categories || (await categories).length == 0) {
+    await CategoryModel.insertMany([
+      { name: "Books" },
+      { name: "Clothing" },
+      { name: "Electronics" },
+    ]);
+  }
+  console.log("Categories added");
+}
