e18ed0ae3af45def4c96edb4ab4ec3eafe9b2dbf
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-07-31 16:17:15 +0530

    updated

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import NoteContext from '../context/notes/noteContext';
 
 export default function NoteItem(props) {
+
     const {note} = props;
+
+      const context = useContext(NoteContext);
+      const {deleteNote} = context
   return (
     <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
           <div className="d-flex align-items-center">
             <h5 className="card-title">{note.title}</h5>
-            <i className="far fa-trash-alt mx-2"></i>
+            <i className="far fa-trash-alt mx-2" onClick={() => {deleteNote(note._id)}}></i>
             <i className="far fa-edit mx-2"></i>
           </div>
           <p className="card-text">{note.description}</p>
--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -92,11 +92,44 @@ const NoteState = (props) => {
     },
   ];
 
-  const [notes, setNotes] =useState(notesInitial)
+  const [notes, setNotes] = useState(notesInitial);
 
+  // Add a Note
+  const addNote = (title, description, tag) => {
+    // TODO: API Call
+    const note = {
+      _id: "61322f119553781a8ca8d0e08",
+      user: "6131dc5e3e4037cd4734a0664",
+      title: title,
+      description: description,
+      tag: tag,
+      date: "2021-09-03T14:20:09.668Z",
+      __v: 0,
+    };
+    setNotes(notes.concat(note));
+  };
+
+  // Delete a Note
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => {
+      return note._id !== id;
+    });
+    setNotes(newNotes);
+  };
+  // Edit a Note
+  const editNote = (id, title, description, tag) => {
+     for (let index = 0; index < notes.length; index++) {
+      const element = notes[index];
+      if (element._id === id) {
+        element.title = title;
+        element.description = description;
+        element.tag = tag;
+      }
+    }
+  };
 
   return (
-    <NoteContext.Provider value={{ notes, setNotes }}>
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
       {props.children}
     </NoteContext.Provider>
   );
